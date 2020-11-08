b3654dfdaa93071c95d5b92891b02a39c76317c6
quangnguyen90 <nguyenphuquang90@gmail.com>
2020-11-08 17:56:02 +0700

    3- Unit test of function countEventNumbers

--- main.test.js
+++ main.test.js
@@ -1,4 +1,4 @@
-import { isOddNumber } from "./main.js";
+import { countEventNumbers, isOddNumber } from "./main.js";
 const expect = chai.expect;
 
 describe("function isOddNumber()", () => {
@@ -10,5 +10,27 @@ describe("function isOddNumber()", () => {
   it("should be false if 2", () => {
     const result = isOddNumber(2);
     expect(result).to.false;
+  });
+});
+
+describe("function countEventNumbers()", () => {
+  it("should equal 0 if null", () => {
+    const result = countEventNumbers(null);
+    expect(result).to.equal(0);
+  });
+
+  it("should equal 0 if []", () => {
+    const result = countEventNumbers([]);
+    expect(result).to.equal(0);
+  });
+
+  it("should equal 1 if [1, 2, 3]", () => {
+    const result = countEventNumbers([1, 2, 3]);
+    expect(result).to.equal(1);
+  });
+
+  it("should equal 2 if [1, 2, 3, 4]", () => {
+    const result = countEventNumbers([1, 2, 3, 4]);
+    expect(result).to.equal(2);
   });
 });
